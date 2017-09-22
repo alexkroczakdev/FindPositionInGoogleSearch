db9a589c2d3745c999f5a27ead77cff7bfea0425
Aleksandra <aleksandra.kroczak@gmail.com>
2017-09-22 19:37:20 +0200

    clean up

--- InfoTrackTask/Controllers/HomeController.cs
+++ InfoTrackTask/Controllers/HomeController.cs
@@ -1,18 +1,11 @@
 ﻿using InfoTrackTask.Logic;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Web;
 using System.Web.Mvc;
 
 namespace InfoTrackTask.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
             return View();
--- InfoTrackTask/Logic/CustomWebClient.cs
+++ InfoTrackTask/Logic/CustomWebClient.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Web;
+﻿using System.Net;
 
 namespace InfoTrackTask.Logic
 {
@@ -22,7 +18,6 @@ namespace InfoTrackTask.Logic
             using (var webClient = new WebClient())
             {
                 result = webClient.DownloadString("http://www.google.com.au/search?&num="+_number+"&q=" + _keyWords);
-
             }
             return result;
         }
